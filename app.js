8961c37eda6489c9d2deb0a08439e725daf6b72a
Guilherme Iria <guilhermeiria@ufrj.br>
2020-11-16 20:20:36 -0300

    Message changed again - Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('F0r4 80z0!'));
+app.get('/', (req, res) => res.send('F0r4 80z0 e ozoto!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
